89834eaa2f299440dc71b05d5081445d615acf29
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2024-01-22 18:49:07 +0600

    add register design

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -6,24 +6,53 @@ const Register = () => {
     const { register, handleSubmit, reset } = useForm();
 
     const onSubmit = (data) => {
+        const name = data.name;
+        const email = data.email;
+        const number = data.number;
+        const roll = data.roll;
+        const password = data.password;
+
+        // get all the data 
+        const allData = {
+            name,
+            email,
+            number,
+            roll,
+            password
+        };
+
+        // post data in mongodb
+        fetch('http://localhost:5000/userPost', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(allData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    Swal.fire({
+                        position: "top-center",
+                        icon: "success",
+                        title: "Your Resister has been successful",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                    reset();
+                    // navigate('/')
+                }
+            })
         console.log(data);
-        Swal.fire({
-            position: "top-center",
-            icon: "Your Resister has been successful",
-            title: "Your work has been saved",
-            showConfirmButton: false,
-            timer: 1500
-        });
-        reset();
     }
 
     return (
-        <div className="bg-[#4070F4]">
+        <div className="bg-gradient-to-r from-blue-200 to-blue-100 backdrop-blur-3xl">
             <div className="container mx-auto py-14">
 
                 <form
                     onSubmit={handleSubmit(onSubmit)}
-                    className="w-[35%] border-2 py-8 px-8 rounded-md shadow-md mx-auto bg-white">
+                    className="w-[35%] border-2 py-8 px-8 rounded-md shadow-sm mx-auto bg-white">
 
                     <div className="text-4xl font-bold mb-8">
                         <h2>Registration</h2>
